0bf550386921c2b8e2540aece0d15f5a00ddcbdd
Vladimir Babin <vovababin@gmail.com>
2017-11-20 19:35:27 +0500

    Forget about tests. Again

--- config/config_test.go
+++ config/config_test.go
@@ -18,7 +18,7 @@ func TestLoadConfiguration(t *testing.T) {
 		PostingKey:             "5...",
 		ActiveKey:              "5...",
 		ReferralFee:            5.0,
-		RequiredVotes:          1,
+		RequiredVotes:          2,
 		InitialUserRating:      10,
 		MaximumOpenedVotes:     3,
 		MaximumUserVotesPerDay: 4,
