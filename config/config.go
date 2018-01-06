4167cfabb2b92774838770195e7a29a6de0699e1
worthless-man <35172777+worthless-man@users.noreply.github.com>
2018-01-07 00:24:20 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -25,6 +25,7 @@ type Config struct {
 	Domains                []string `json:"domains"`
 	Chain                  string   `json:"chain"`
 	Rpc                    []string `json:"rpc"`
+	tester		       int
 }
 
 func LoadConfiguration(file string, config *Config) error {
