72a587f19bf1ad1e74e9cdabf3275a29948abd36
Vladimir Babin <vovababin@gmail.com>
2018-01-09 12:24:46 +0500

    Check vesting shares (#52)
    * Check vesting shares
    
    * Remove unnecessary lines
    
    * Working with huge numbers

--- vendor/github.com/asuleymanov/golos-go/client/verify.go
+++ vendor/github.com/asuleymanov/golos-go/client/verify.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"strconv"
+	"math/big"
 
 	// Vendor
 	"github.com/pkg/errors"
@@ -120,12 +122,44 @@ func (api *Client) Verify_Post(author, permlink string) bool {
 func (api *Client) Verify_Delegate_Posting_Key_Sign(username string, arr []string) []string {
 	var truearr []string
 
+	props, err := api.Rpc.Database.GetDynamicGlobalProperties()
+	if err != nil {
+		log.Println(errors.Wrap(err, "Error Get Dynamic Global Properties"))
+		return nil
+	}
+	totalVestingShares, err := strconv.ParseFloat(strings.Split(props.TotalVestingShares, " ")[0], 64)
+	if err != nil {
+		log.Println(errors.Wrap(err, "Error Parse Total Vesting Shares"))
+		return nil
+	}
+	totalVestingSharesInt, _ := new(big.Float).SetFloat64(totalVestingShares).Int(nil)
+	maxVirtualBandwidth := props.MaxVirtualBandwidth.Int
+
 	acc, err := api.Rpc.Database.GetAccounts(arr)
 	if err != nil {
 		log.Println(errors.Wrapf(err, "Error Verify Delegate Vote Sign: "))
 		return nil
 	} else {
 		for _, val := range acc {
+			if !val.CanVote {
+				continue
+			}
+			vestingShares, err := strconv.ParseFloat(strings.Split(val.VestingShares, " ")[0], 64)
+			if err != nil {
+				log.Println(errors.Wrap(err, "Error Parse Vesting Shares"))
+				continue
+			}
+			if vestingShares < 0 {
+				continue
+			}
+			vestingSharesInt, _ := new(big.Float).SetFloat64(vestingShares).Int(nil)
+			firstCondition := new(big.Int).Mul(vestingSharesInt, maxVirtualBandwidth)
+			secondCondition := new(big.Int).Mul(val.AverageBandwidth.Int, totalVestingSharesInt)
+			log.Printf("Разность: %s", new(big.Int).Sub(firstCondition, secondCondition).String())
+			if firstCondition.Cmp(secondCondition) == -1 {
+				log.Printf("Аккаунт %s has exceeded maximum allowed bandwidth per vesting share", val.Name)
+				continue
+			}
 			for _, v := range val.Posting.AccountAuths {
 				l := strings.Split(strings.Replace(strings.Replace(fmt.Sprintf("%v", v), "[", "", -1), "]", "", -1), " ")[0]
 				if l == username {
