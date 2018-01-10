d5f92def63291f7d5df740980447103aa3052458
worthless-man <35172777+worthless-man@users.noreply.github.com>
2018-01-11 01:27:42 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -29,6 +29,7 @@ type Config struct {
 	Repository	       string 	`json:"repository"`
 	IgnoreVP	       bool    	`json:"ignore_vp"`
 	BannedTags	       []string	`json:"banned_tags"` 
+	Censorship	       bool    	`json:"censorship"`
 }
 
 func LoadConfiguration(file string, config *Config) error {
