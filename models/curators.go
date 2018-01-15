2bacb44f379b4a5b62bf4891b5f07bca741f84f2
worthless-man <35172777+worthless-man@users.noreply.github.com>
2018-01-15 21:41:34 +0300

    Update curators.go

--- models/curators.go
+++ models/curators.go
@@ -2,52 +2,54 @@ package models
 
 import "database/sql"
 
-type Referral struct {
-	UserID    int
-	Referrer  string
-	Completed bool
-}
-
-func (referral Referral) Save(db *sql.DB) (bool, error) {
+func NewCurator(userID int, chatID int, db *sql.DB) (bool, error) {
 	prepare, err := db.Prepare("INSERT INTO referrals(" +
 		"user_id," +
-		"referrer," +
-		"completed) " +
+		"chat_id," +
+		"active" +
 		"values(?, ?, ?)")
 	defer prepare.Close()
 	if err != nil {
 		return false, err
 	}
-	_, err = prepare.Exec(
-		referral.UserID,
-		referral.Referrer,
-		referral.Completed)
+	_, err = prepare.Exec(userID, chatID, true)
 	if err != nil {
 		return false, err
 	}
 	return true, nil
 }
 
-func (referral Referral) SetCompleted(db *sql.DB) error {
-	_, err := db.Exec("UPDATE referrals SET completed = 1 WHERE user_id = ?", referral.UserID)
+func DeactivateCurator(userID int, db *sql.DB) error {
+	_, err := db.Exec("UPDATE curators SET active = 0 WHERE user_id = ?", userID)
 	return err
 }
 
-func GetReferralByUserID(userID int, db *sql.DB) (referral Referral, err error) {
-	row := db.QueryRow("SELECT user_id, referrer, completed FROM referrals WHERE user_id = ?", userID)
-	err = row.Scan(&referral.UserID, &referral.Referrer, &referral.Completed)
-	return referral, err
-}
-
-func IsReferrerExists(referrer string, db *sql.DB) bool {
-	row := db.QueryRow("SELECT user_id FROM referrals "+
-		"WHERE referrer = ?", referrer)
-	var userID *int
-	row.Scan(&userID)
-	return userID != nil
-}
-
-func REFchangeUserID(db *sql.DB, oldID int, newID int) error {
-	_, err := db.Exec("UPDATE referrals SET user_id = ? WHERE user_id = ?", newID, oldID)
+func ActivateCurator(userID int, db *sql.DB) error {
+	_, err := db.Exec("UPDATE curators SET active = 1 WHERE user_id = ?", userID)
 	return err
+}
+
+func GetLastCuratorVotes(userID int, db *sql.DB) (int, error) {
+	row := db.QueryRow("SELECT last_votes WHERE user_id = ?", userID)
+	var result *int
+	err = row.Scan(&result)
+	return result, err
+}
+
+func IncrementCuratorVotes(userID int, db *sql.DB) {
+	_, err := db.Exec("UPDATE curators SET (total_votes, last_votes) = (SELECT total_votes, last_votes WHERE user_id = ?) WHERE user_id = ?", userID, userID)
+	return err
+}
+
+func IsCuratorExists(userID int, db *sql.DB) bool {
+	row := db.QueryRow("SELECT user_id FROM curators "+
+		"WHERE user_id = ?", userID)
+	var result *int
+	row.Scan(&result)
+	return result != nil
+}
+
+func CleanAllLastVotes(db *sql.DB) {
+	_, err := db.Exec("UPDATE curators SET last_votes = 0")
+	return err	
 }
