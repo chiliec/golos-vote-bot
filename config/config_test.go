3266f12ba52d1273cd8f0718a52aa9a506c1f2c9
worthless-man <35172777+worthless-man@users.noreply.github.com>
2018-01-18 02:16:40 +0300

    Update config_test.go

--- config/config_test.go
+++ config/config_test.go
@@ -36,8 +36,6 @@ func TestLoadConfiguration(t *testing.T) {
 		IgnoreVP:               true,
 		BannedTags:             []string{"test", "test1"},
 		Censorship:             false,
-		VotingDelay:		60,
-		PoliceDelay:		3,
 	}
 	if !reflect.DeepEqual(defaultConfig, config) {
 		t.Error("Конфиги не совпадают")
