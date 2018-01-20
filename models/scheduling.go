43bf7c990a348e8467f92ece92e64c311f6d3b79
worthless-man <35172777+worthless-man@users.noreply.github.com>
2018-01-21 00:59:31 +0300

    Update scheduling.go

--- models/scheduling.go
+++ models/scheduling.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-function WannaSleepOneDay(hour, min int) (sleepTime time.Duration) {
+func WannaSleepOneDay(hour, min int) (sleepTime time.Duration) {
         now := time.Now()
         nowYear, nowMonth, nowDay := now.Date()
         nextDate := time.Date(nowYear, nowMonth, nowDay + 1, hour, min, 0, 0, now.Location())
