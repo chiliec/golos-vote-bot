bfbe3d2d70261ffdef0ab4e4b7db98a153fb81e4
worthless-man <35172777+worthless-man@users.noreply.github.com>
2018-01-21 00:59:16 +0300

    Update scheduling.go

--- models/scheduling.go
+++ models/scheduling.go
@@ -1,1 +1,13 @@
+package models
 
+import (
+	"database/sql"
+	"time"
+)
+
+function WannaSleepOneDay(hour, min int) (sleepTime time.Duration) {
+        now := time.Now()
+        nowYear, nowMonth, nowDay := now.Date()
+        nextDate := time.Date(nowYear, nowMonth, nowDay + 1, hour, min, 0, 0, now.Location())
+        return nextDate.Sub(time.Now())
+}
